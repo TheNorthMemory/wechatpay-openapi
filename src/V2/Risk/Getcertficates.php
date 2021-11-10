1438cfeb4b10f22cf7fe25ec2e4ad8e5ed2fb076
James ZHNAG <thenorthmemory@qq.com>
2021-11-10 20:08:02 +0800

    `/risk/getcertficates`

---
+++ src/V2/Risk/Getcertficates.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);/* Generated file. DO NOT EDIT! */
+
+namespace WeChatPay\OpenAPI\V2\Risk;
+
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Promise\PromiseInterface;
+
+/**
+ * 这是一个纯接口描述文件，由程序自动生成，旨在为IDE提供结构提示，无他用。
+ */
+interface Getcertficates
+{
+    /**
+     * 获取RSA加密公钥(同步模式)
+     * @param array<string,mixed> $options
+     * @link https://pay.weixin.qq.com/wiki/doc/api/xiaowei.php?chapter=19_11
+     */
+    public function post(array $options = [
+        'xml' => [
+            'mch_id' => '1900000109',
+            'sign_type' => 'HMAC-SHA256',
+        ],
+    ]): ResponseInterface;
+
+    /**
+     * 获取RSA加密公钥(异步模式)
+     * @param array<string,mixed> $options
+     * @link https://pay.weixin.qq.com/wiki/doc/api/xiaowei.php?chapter=19_11#async
+     */
+    public function postAsync(array $options = [
+        'xml' => [
+            'mch_id' => '1900000109',
+            'sign_type' => 'HMAC-SHA256',
+        ],
+    ]): PromiseInterface;
+}

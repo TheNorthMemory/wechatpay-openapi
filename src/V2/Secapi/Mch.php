0bfbad1a82f506116902e0734c3048c22499aa44
James ZHNAG <thenorthmemory@qq.com>
2023-01-24 08:48:17 +0800

    `v2/secapi/mch/channelsetting.yaml`

--- src/V2/Secapi/Mch.php
+++ src/V2/Secapi/Mch.php
@@ -5,6 +5,7 @@ namespace WeChatPay\OpenAPI\V2\Secapi;
 /**
  * @property-read Mch\_addinstitutionsub_ $_addinstitutionsub_
  * @property-read Mch\Addsubdevconfig $addsubdevconfig
+ * @property-read Mch\Channelsetting $channelsetting
  * @property-read Mch\_modifyinstitutionsub_ $_modifyinstitutionsub_
  * @property-read Mch\Modifymchinfo $modifymchinfo
  * @property-read Mch\_queryinstitutionsub_ $_queryinstitutionsub_
---
+++ src/V2/Secapi/Mch/Channelsetting.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);/* Generated file. DO NOT EDIT! */
+
+namespace WeChatPay\OpenAPI\V2\Secapi\Mch;
+
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Promise\PromiseInterface;
+
+/**
+ */
+interface Channelsetting
+{
+    /**
+     * 银行服务商特约子商户指定渠道号(同步模式)
+     * @param array<string,mixed> $options
+     * @link https://pay.weixin.qq.com/wiki/doc/api/mch_bank.php?chapter=9_26
+     */
+    public function post(array $options = [
+        'xml' => [
+            'appid' => 'wx931386123456789e',
+            'mch_id' => '1451234567',
+            'sub_mch_id' => '10000101',
+            'channel_id' => '10000102',
+        ],
+        'nonceless' => true,
+        'security' => true,
+    ]): ResponseInterface;
+
+    /**
+     * 银行服务商特约子商户指定渠道号(异步模式)
+     * @param array<string,mixed> $options
+     * @link https://pay.weixin.qq.com/wiki/doc/api/mch_bank.php?chapter=9_26#async
+     */
+    public function postAsync(array $options = [
+        'xml' => [
+            'appid' => 'wx931386123456789e',
+            'mch_id' => '1451234567',
+            'sub_mch_id' => '10000101',
+            'channel_id' => '10000102',
+        ],
+        'nonceless' => true,
+        'security' => true,
+    ]): PromiseInterface;
+}

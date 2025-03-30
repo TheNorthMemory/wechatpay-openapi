22bbfdac3867023c7506b048acc9d3f3da52cae1
James ZHNAG <thenorthmemory@qq.com>
2025-03-30 10:17:19 +0800

    U: `v2/papay/*`

--- src/V2/Papay/Entrustweb.php
+++ src/V2/Papay/Entrustweb.php
@@ -14,12 +14,40 @@ interface Entrustweb
      * @param array<string,mixed> $options
      * @link https://wechatpay.im/openapi/v2/papay/entrustweb
      */
-    public function get(array $options = []): ResponseInterface;
+    public function get(array $options = [
+        'query' => [
+            'appid' => 'wxcbda96de0b165486',
+            'mch_id' => '1200009811',
+            'plan_id' => '12535',
+            'contract_code' => '100000',
+            'request_serial' => '1000',
+            'contract_display_account' => '微信代扣',
+            'notify_url' => 'https://weixin.qq.com',
+            'version' => '1.0',
+            'sign' => 'C380BEC2BFD727A4B6845133519F3AD6',
+            'timestamp' => '1414488825',
+            'return_web' => '1',
+        ],
+    ]): ResponseInterface;
 
     /**
      * 公众号纯签约(异步模式)
      * @param array<string,mixed> $options
      * @link https://wechatpay.im/openapi/v2/papay/entrustweb
      */
-    public function getAsync(array $options = []): PromiseInterface;
+    public function getAsync(array $options = [
+        'query' => [
+            'appid' => 'wxcbda96de0b165486',
+            'mch_id' => '1200009811',
+            'plan_id' => '12535',
+            'contract_code' => '100000',
+            'request_serial' => '1000',
+            'contract_display_account' => '微信代扣',
+            'notify_url' => 'https://weixin.qq.com',
+            'version' => '1.0',
+            'sign' => 'C380BEC2BFD727A4B6845133519F3AD6',
+            'timestamp' => '1414488825',
+            'return_web' => '1',
+        ],
+    ]): PromiseInterface;
 }
--- src/V2/Papay/H5entrustweb.php
+++ src/V2/Papay/H5entrustweb.php
@@ -14,12 +14,44 @@ interface H5entrustweb
      * @param array<string,mixed> $options
      * @link https://wechatpay.im/openapi/v2/papay/h5entrustweb
      */
-    public function get(array $options = []): ResponseInterface;
+    public function get(array $options = [
+        'query' => [
+            'appid' => 'wxcbda96de0b165486',
+            'mch_id' => '1200009811',
+            'plan_id' => '12535',
+            'contract_code' => '100000',
+            'request_serial' => '1000',
+            'contract_display_account' => '微信代扣',
+            'notify_url' => 'https://weixin.qq.com',
+            'version' => '1.0',
+            'sign' => 'C380BEC2BFD727A4B6845133519F3AD6',
+            'sign_type' => 'HMAC-SHA256',
+            'timestamp' => '1414488825',
+            'clientip' => '119.145.83.6',
+            'return_appid' => 'wxcbda96de0b165486',
+        ],
+    ]): ResponseInterface;
 
     /**
      * H5纯签约(异步模式)
      * @param array<string,mixed> $options
      * @link https://wechatpay.im/openapi/v2/papay/h5entrustweb
      */
-    public function getAsync(array $options = []): PromiseInterface;
+    public function getAsync(array $options = [
+        'query' => [
+            'appid' => 'wxcbda96de0b165486',
+            'mch_id' => '1200009811',
+            'plan_id' => '12535',
+            'contract_code' => '100000',
+            'request_serial' => '1000',
+            'contract_display_account' => '微信代扣',
+            'notify_url' => 'https://weixin.qq.com',
+            'version' => '1.0',
+            'sign' => 'C380BEC2BFD727A4B6845133519F3AD6',
+            'sign_type' => 'HMAC-SHA256',
+            'timestamp' => '1414488825',
+            'clientip' => '119.145.83.6',
+            'return_appid' => 'wxcbda96de0b165486',
+        ],
+    ]): PromiseInterface;
 }
